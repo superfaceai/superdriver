6a3b7b08e66b144bba620ddc815ebd4849cf144f
Zdenek Nemec <zdenek@me.com>
2019-11-28 10:01:07 +0100

    Register handles registering and unregistering of services

--- src/register.js
+++ src/register.js
@@ -1,13 +1,17 @@
-import fetch from 'isomorphic-fetch'
+import fetch from 'isomorphic-fetch';
+import Debug from 'debug';
+const debug = Debug('superdriver:register');
+
+const CONFLICT_RELATION_KEY = 'conflictUrl';
 
 export class Register {
   /**
    * Service Register proxy
    *
-   * @param {String} registerURL URL of the register to use
+   * @param {String} registerUrl URL of the register to use
    */
-  constructor(registerURL) {
-    this.registerURL = registerURL;
+  constructor(registerUrl) {
+    this.registerUrl = registerUrl;
   }
 
   /**
@@ -16,26 +20,118 @@ export class Register {
    * @param {String} profileId Id of the profile the matching sarvice has to support
    */
   async findServices(profileId) {
-    try {
-      const response = await fetch(
-        `${this.registerURL}/find?semanticProfile=${encodeURIComponent(profileId)}`,
+    const response = await fetch(
+      `${this.registerUrl}/api/registry?semanticProfile=${encodeURIComponent(profileId)}`,  // TODO: use superdriver instead of hard-coding URLs
+      {
+        headers: {
+          'Accept': 'application/json'
+        },
+        redirect: 'follow',
+      }
+    )
+
+    const body = await response.json()
+    const services = body['disco'];
+
+    if (!services || !services.length) {
+      return Promise.reject(`No service for profile '${profileId}' found.`)
+    }
+
+    return services;
+  }
+
+  /**
+   * Register a new service at the registry
+   *
+   * @param {Object} param0 Service details* 
+   * @param {string} param0.serviceUrl Url of the service being registered
+   * @param {string} param0.mappingUrl Url of the mapping for the registered service
+   * @param {string} param0.semanticProfile Id of the semantic profile
+   * @returns {object} Representation of registered service
+   */
+  async registerService({ serviceUrl, mappingUrl, semanticProfile }) {
+    debug('registering service', serviceUrl)
+    const response = await fetch(
+      `${this.registerUrl}/api/registry`,
+      {
+        method: 'POST',
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        redirect: 'follow',
+        body: JSON.stringify({ serviceUrl, mappingUrl, semanticProfile })
+      }
+    )
+
+    const body = await response.json()
+    // Return created service
+    if (response.ok) {
+      return {
+        result: 'created',
+        service: body
+      };
+    }
+
+    // Handle conflicts
+    if (response.status == 409 && (CONFLICT_RELATION_KEY in body)) {
+      const conflictUrl = body[CONFLICT_RELATION_KEY]
+      if (!conflictUrl) {
+        return Promise.reject({ result: 'failed', status: response.status, detail: body })
+      }
+
+      // Fetch conflicting service
+      debug('fetching conflicting service ', conflictUrl)
+      const conflictResponse = await fetch(
+        `${this.registerUrl}${conflictUrl}`,
         {
           headers: {
             'Accept': 'application/json'
-          }
+          },
+          redirect: 'follow',
         }
       )
-
-      const body = await response.json()
-      const services = body['disco'];
-
-      if (!services || !services.length) {
-        throw(`No service for profile '${profileId}' found.`)
+      // Return created service
+      const conflictBody = await conflictResponse.json()
+      if (conflictResponse.ok) {
+        return {
+          result: 'conflict',
+          service: conflictBody
+        };
       }
 
-      return services;
-    } catch(error) {
-      throw(`Error while fetching profile ${profileId}: ${error.message}`)
+      return Promise.reject({ result: 'failed-conflict', status: conflictResponse.status, detail: conflictBody })
     }
+
+    return Promise.reject({ result: 'failed', status: response.status, detail: body })
+  }
+
+  /**
+   *  Unregister a previous registered service
+   * 
+   * @param {Object} param0 Service details
+   */
+  async unregisterService({ serviceUrl }) {
+    debug('unregistering service', serviceUrl)
+
+    const response = await fetch(
+      `${this.registerUrl}${serviceUrl}`,
+      {
+        method: 'DELETE',
+        cache: 'no-cache',
+        headers: {
+          'Accept': 'application/json'
+        },
+        redirect: 'follow'
+      }
+    )
+
+    if (response.ok) {
+      return
+    }
+
+    const body = await response.json()
+    return Promise.reject({ result: 'failed', status: response.status, detail: body })
   }
 }
